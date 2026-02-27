d049e9fe205856af37f817dcb23c407fcf3be15f
KOIexe <72695755+KOIexe86@users.noreply.github.com>
2026-02-27 19:17:19 +0100

    Added main files

---
+++ script.js
@@ -0,0 +1,6 @@
+
+// Boutton GitHub dans la section de présentation
+
+document.getElementById('github').addEventListener('click', function() {
+    window.open('https://github.com/KOIexe86', '_blank');
+});
