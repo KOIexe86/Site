4239985ae36bc0e9c8b6cc950f0f3cfb39f7cd77
KOIexe <72695755+KOIexe86@users.noreply.github.com>
2026-03-07 09:06:35 +0100

    Added the real description for projects, and a drawing of a car

--- script.js
+++ script.js
@@ -5,18 +5,23 @@ document.getElementById('github').addEventListener('click', function() {
     window.open('https://github.com/KOIexe86', '_blank');
 });
 
+// Boutton "By: Nithawi" comme crédit de la voiture
+document.getElementById('nithawi').addEventListener('click', function() {
+    window.open('https://www.instagram.com/nithawi_draw?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw==', '_blank');
+});
+
 
 // Cards
 
 const buttonts = document.querySelectorAll(".Card-Btn");
-const slides = document.querySelectorAll(".Slide");
-const descriptions = document.querySelectorAll(".Description");
+const slides = document.querySelectorAll(".slide");
+const descriptions = document.querySelectorAll(".description");
 
 buttonts.forEach((button) => {
     button.addEventListener("click", (e) => {
         const calcNextSlide = e.target.id === "next" ? 1 : -1;
         const slideActuel = document.querySelector(".active");
-        const descriptionActuel = document.querySelector(".Description.active");
+        const descriptionActuel = document.querySelector(".description.active");
 
         newIndex = calcNextSlide + [...slides].indexOf(slideActuel);
         
